fd0ffcafbaeb73335b75a0334480b077fe79c659
wedojava <wedojava@gmail.com>
2020-04-15 11:51:38 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Install youtube_dl
+
+```bash
+pip install youtube_dl
+```
+
+# Usage
+
+Just execute and enter the number you want.
---
+++ youtube_dl_cli.py
@@ -0,0 +1,56 @@
+# from __future__ import unicode_literals
+import os
+import re
+
+import youtube_dl
+
+
+def main():
+    print("+--------------------------+")
+    print("+ [1] 下载 list.txt 里的东西 +")
+    print("+ [2] 直接给地址下载         +")
+    a = input("+ 请选择：                  +")
+    if a == "1":
+        download(get_urls("list.txt"))
+    if a == "2":
+        while True:
+            b = []
+            b.append(input("输入视频所在网页地址："))
+            download(b)
+
+
+def download(dl_list: list):
+    ydl_opts = {
+        'format': 'bestvideo[height <=? 1080][ext=mp4]+bestaudio[ext=m4a]/best[height <=? 1080]/best',
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(dl_list)
+        # ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+
+
+def get_urls(filepath: str) -> list:
+    """Get url list from file. Just urls without other text."""
+
+    l_urls = _list = []
+    if not os.path.lexists(filepath):
+        return None
+    try:
+        with open(filepath, encoding='gbk') as f:
+            _list = f.read().splitlines()
+    except Exception:
+        with open(filepath, encoding='utf8') as f:
+            _list = f.read().splitlines()
+
+    regex = r"([^\\/:*?\"<>|!]*)[|]?(https://.*)"
+    for i in _list:
+        if i == '':
+            continue
+        else:
+            m = re.search(regex, i)
+            l_urls.append(m.group(2))
+
+    return l_urls
+
+
+if __name__ == '__main__':
+    main()
